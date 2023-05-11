1dd10465c108e1330c07ffe0660acfd282f3df5a
Own3Dqq <own3dqq@gmail.com>
2023-05-11 11:48:23 +0300

    implemented methods generateFood

--- js/index.js
+++ js/index.js
@@ -1,155 +1,194 @@
 import Grid from './grid.js';
-import { DIRECTIONS as DR} from './helpers.js';
+import { DIRECTIONS as DR } from './helpers.js';
 
 class Snake extends Grid {
-    static snakeCellCssClass = 'snake-cell';
-    static snakeCssClass = 'snake';
-    static snakeHeadCssClass = 'snake-head';
-    static snakeBodyCssClass = 'snake-body';
-    static gridContainerCssSelector = '#snake-container';
+	static snakeCellCssClass = 'snake-cell';
+	static snakeCssClass = 'snake';
+	static snakeHeadCssClass = 'snake-head';
+	static snakeBodyCssClass = 'snake-body';
+	static gridContainerCssSelector = '#snake-container';
 
-    #snake = [];
-    #process = null;
-    #score = 0;
-    #speed = 0;
-    #food = null;
-    #controls = this.find('#snake-controls-form');
-    #startBtn = this.find('#snake-start-game');
-    #endBtn = this.find('#snake-end-game');
-    #messageContainer = this.find('#snake-message');
-    #scoreContainer = this.find('#snake-score');
+	#snake = [];
+	#process = null;
+	#score = 0;
+	#speed = 0;
+	#food = '../img/apple.png';
+	#controls = this.find('#snake-controls-form');
+	#startBtn = this.find('#snake-start-game');
+	#endBtn = this.find('#snake-end-game');
+	#messageContainer = this.find('#snake-message');
+	#scoreContainer = this.find('#snake-score');
 
-    constructor({boxSize, gridCount}) {
-        super({
-            boxSize,
-            gridCount, 
-            gridCellCssClass: Snake.snakeCellCssClass,
-            gridContainerSelector: Snake.gridContainerCssSelector,
-        });
-        this.direction = DR.LEFT;
+	constructor({ boxSize, gridCount }) {
+		super({
+			boxSize,
+			gridCount,
+			gridCellCssClass: Snake.snakeCellCssClass,
+			gridContainerSelector: Snake.gridContainerCssSelector,
+		});
+		this.direction = DR.LEFT;
 
-        this.#init();
-    }
+		this.#init();
+	}
 
-    #init() {
-        document.addEventListener('keydown', (event) => this.#updateDirection(event));
+	#init() {
+		document.addEventListener('keydown', (event) => this.#updateDirection(event));
 
-        this.#startBtn.addEventListener('click', (event) => this.#start(event));
-        this.#endBtn.addEventListener('click', (event) => this.#end(event));
-    }
+		this.#startBtn.addEventListener('click', (event) => this.#start(event));
+		this.#endBtn.addEventListener('click', (event) => this.#end(event));
+	}
 
-    #start() {
-        this.#snake = this.#buildSnake(Math.floor(this.gridCount / 2), Math.floor(this.gridCount / 2));
-        // #generateFood() // place img in a random cell 
+	#start() {
+		this.#snake = this.#buildSnake(Math.floor(this.gridCount / 2), Math.floor(this.gridCount / 2));
+		this.#generateFood(Snake.gridContainerCssSelector);
+		this.#speed = +this.#controls.speed.value;
+		this.#messageContainer.innerHTML = 'Welcome to Snake !';
+		this.#startBtn.style.display = 'none';
+		this.#endBtn.style.display = 'block';
 
-        this.#speed = +this.#controls.speed.value;
-        this.#messageContainer.innerHTML = 'Welcome to Snake !';
-        // hide button
-        // endBtn => display block
-        // startBtn => display none
+		this.#process = setInterval(() => {
+			let { cell, row } = this.#snake[0];
+			// let { cell, row } = #noWallMode();
 
-        this.#process = setInterval(() => {
-          
-            let { cell, row } = this.#snake[0];
-            // let { cell, row } = #noWallMode();
+			switch (this.direction) {
+				case DR.LEFT:
+					{
+						this.#snake.unshift({
+							cell: cell - 1,
+							row,
+						});
+					}
+					break;
 
-            switch(this.direction) {
-                case DR.LEFT: {
-                    this.#snake.unshift({
-                        cell: cell - 1,
-                        row
-                    });
-                }; break;
+				case DR.RIGHT:
+					{
+						this.#snake.unshift({
+							cell: cell + 1,
+							row,
+						});
+					}
+					break;
 
-                case DR.RIGHT: {
-                    this.#snake.unshift({
-                        cell: cell + 1,
-                        row,
-                    });
-                }; break;
+				case DR.UP:
+					{
+						this.#snake.unshift({
+							cell,
+							row: row - 1,
+						});
+					}
+					break;
 
-                case DR.UP: {
-                    this.#snake.unshift({
-                        cell,
-                        row: row - 1,
-                    });
-                }; break;
+				case DR.DOWN:
+					{
+						this.#snake.unshift({
+							cell,
+							row: row + 1,
+						});
+					}
+					break;
+			}
 
-                case DR.DOWN: {
-                    this.#snake.unshift({
-                        cell,
-                        row: row + 1,
-                    });
-                }; break;
-            }
+			this.#clear();
+			this.#update();
+		}, this.#speed);
+	}
 
-            this.#clear();
-            this.#update();
+	#checkHasFoodEaten() {}
 
-        }, this.#speed);
+	#update() {
+		// if (this.checkHasFoodEaten()) {
+		// 	this.#generateFood();
+		// }
+		// checkHasFoodEaten();
+		// if a snake has eaten apple then add +1 to score and push one more object ( {cell, row} )
+		// after it ate apple you should generate new coords for the apple to append it in a cell again
 
-    }
+		// checkOnTailCrash - if a head bump into the tail. if so need to final game
+		// this.#message.innerHTML = 'Game Over', reset score and so on
 
-    #update() {
-        // checkHasFoodEaten() 
-        // if a snake has eaten apple then add +1 to score and push one more object ( {cell, row} )
-        // after it ate apple you should generate new coords for the apple to append it in a cell again 
+		this.#snake.pop();
 
+		for (const [index, snakeData] of this.#snake.entries()) {
+			let cellElement = this.#findByCoords(snakeData);
+			if (index === 0) {
+				cellElement.classList.add(Snake.snakeHeadCssClass, Snake.snakeCssClass);
+			} else {
+				cellElement.classList.add(Snake.snakeBodyCssClass, Snake.snakeCssClass);
+			}
+		}
+	}
 
-        // checkOnTailCrash - if a head bump into the tail. if so need to final game 
-        // this.#message.innerHTML = 'Game Over', reset score and so on 
+	#clear() {
+		let cells = this.find(`.${Snake.snakeCssClass}`, this.gridContainer);
 
-        this.#snake.pop();
+		cells.forEach((cell) => {
+			cell.className = Snake.snakeCellCssClass;
+		});
+	}
 
-        for (const [index, snakeData] of this.#snake.entries()) {
-            let cellElement = this.#findByCoords(snakeData);
-            console.log(cellElement);
-            if(index === 0) {
-                cellElement.classList.add(Snake.snakeHeadCssClass, Snake.snakeCssClass);
-            } else {
-                cellElement.classList.add(Snake.snakeBodyCssClass, Snake.snakeCssClass);
-            }
-        }
-    }
+	#updateDirection(event) {
+		let key = event.key;
 
-    #clear() {
-        let cells = this.find(`.${Snake.snakeCssClass}`, this.gridContainer);
-        
-        cells.forEach(cell => {
-            cell.className = Snake.snakeCellCssClass;
-            console.log('cell', cell.className);
-        });
-    }
-    
-    #updateDirection(event) {
-        let key = event.key;
-        console.log(event);
+		if (key === 'ArrowLeft' && this.direction != DR.RIGHT) this.direction = DR.LEFT;
+		else if (key === 'ArrowUp' && this.direction != DR.DOWN) this.direction = DR.UP;
+		else if (key === 'ArrowRight' && this.direction != DR.LEFT) this.direction = DR.RIGHT;
+		else if (key === 'ArrowDown' && this.direction != DR.UP) this.direction = DR.DOWN;
+	}
 
-        if(key === 'ArrowLeft' && this.direction != DR.RIGHT) this.direction = DR.LEFT;
-        else if(key === 'ArrowUp' && this.direction != DR.DOWN) this.direction = DR.UP;
-        else if(key === 'ArrowRight' && this.direction != DR.LEFT) this.direction = DR.RIGHT;
-        else if(key === 'ArrowDown' && this.direction != DR.UP) this.direction = DR.DOWN;
-    }
-    
-    // Supplement the method with additional implementation like reseting all data (like score, buttons state, ...)
-    #end() {
-        clearInterval(this.#process);
+	#generateFood() {
+		const container = document.querySelector(Snake.gridContainerCssSelector);
+		const food = new Image(this.boxSize, this.boxSize);
+		food.src = './img/apple.png';
 
-        this.#clear();
-    }
+		function randomCell() {
+			const randCell = Math.floor(Math.random() * container.children.length);
+			if (
+				container.children[randCell].classList.contains('snake-head') ||
+				container.children[randCell].classList.contains('snake-head')
+			) {
+				randomCell();
+			} else {
+				container.children[randCell].append(food);
+			}
+		}
 
-    #findByCoords({cell, row}) {
-        return this.find(`[data-cell="${cell}"][data-row="${row}"]`, this.gridContainer);
-    }
+		randomCell();
+	}
 
-    #buildSnake(startCell, startRow, size = 5) {
-        return new Array(size).fill(null)
-                              .map( (value, index) => ({ row: startRow, cell: startCell + index}))
-    }
+	#resetData() {
+		this.#score = 0;
+		this.#startBtn.style.display = 'block';
+		this.#endBtn.style.display = 'none';
+		const snakeContainer = document.querySelector(Snake.gridContainerCssSelector);
+		(function () {
+			for (let i = 0; i < snakeContainer.children.length; i++) {
+				const element = snakeContainer.children[i];
+				element.innerHTML = '';
+			}
+		})();
+	}
 
+	// Supplement the method with additional implementation like reseting all data (like score, buttons state, ...)
+
+	#end() {
+		clearInterval(this.#process);
+
+		this.#clear();
+		this.#resetData();
+	}
+
+	#findByCoords({ cell, row }) {
+		return this.find(`[data-cell="${cell}"][data-row="${row}"]`, this.gridContainer);
+	}
+
+	#buildSnake(startCell, startRow, size = 5) {
+		return new Array(size).fill(null).map((value, index) => ({ row: startRow, cell: startCell + index }));
+	}
 }
 
 new Snake({
-    boxSize: 30,
-    gridCount: 12
-})
+	boxSize: 44,
+	gridCount: 12,
+});
+
+console.log();
